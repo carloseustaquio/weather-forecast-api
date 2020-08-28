948efaa9cbedb5fdea4339e48d56fe15a3a078a1
Carlos Eustáquio <caeu227@gmail.com>
2020-08-27 22:31:44 -0300

    adjus tests

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,10 +1,7 @@
-import supertest from 'supertest'
-
 describe('Beach forecast functional tests', () => {
   it("should return a forecast with just a few times", async () => {
-    const { body, status } = await supertest(app).get('/forecast')
+    const { body, status } = await global.testRequest.get('/forecast')
     expect(status).toBe(200)
-    expect(body).toBe([{ "forecast": 1234 }])
+    expect(body).toEqual([{ "forecast": 1234 }])
   })
-
 })
